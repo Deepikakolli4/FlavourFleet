f2bd8304059d4a00f97f5171f3acbfb355e03aac
Deepikakolli4 <deepikakolli2004@gmail.com>
2025-05-01 11:24:15 +0530

    working in frontend

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import LandingPage from './vendorDashBoard/pages/LandingPage';
+
+function App() {
+  return (
+    <div >
+      <h1> WELCOME TO FLAVOUR FEET</h1>
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/vendorDashBoard/pages/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+
+const LandingPage = () => {
+  return (
+   <>
+   <section className='landingSection'>
+    <NavBar/>
+   </section>
+   </>
+  )
+}
+
+export default LandingPage
