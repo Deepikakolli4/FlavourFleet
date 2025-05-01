7e42265689275d96379d44f5352fbd52c03f5a94
Deepikakolli4 <deepikakolli2004@gmail.com>
2025-05-01 12:29:57 +0530

    side bar completed

--- client/src/vendorDashBoard/pages/LandingPage.js
+++ client/src/vendorDashBoard/pages/LandingPage.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import NavBar from '../components/NavBar'
+import NavBar from '../components/NavBar/NavBar'
+import SideBar from '../components/SideBar/SideBar'
 
 const LandingPage = () => {
   return (
    <>
    <section className='landingSection'>
     <NavBar/>
+    <SideBar/>
    </section>
    </>
   )
