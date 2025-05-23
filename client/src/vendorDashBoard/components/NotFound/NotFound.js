80120a572fae987dad6e40be240e7755cb4006e9
Deepikakolli4 <deepikakolli2004@gmail.com>
2025-05-23 14:45:37 +0530

    fixed up

--- client/src/vendorDashBoard/components/NotFound/NotFound.js
+++ client/src/vendorDashBoard/components/NotFound/NotFound.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import gif from './giphy.gif';
 import './NotFound.css';
+
 const NotFound = () => {
   return (
-    <div className='errorSection'>
-        <h1>404</h1>
-         <div className="gifContainer">
-                <img src={gif} alt="pagenotfound" />
-          </div>
+    <div className="errorSection">
+      <h1>404</h1>
+      <div className="gifContainer">
+        <img src={gif} alt="pagenotfound" />
+      </div>
+      <Link to="/">
+        <p>Go Back</p>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default NotFound
+export default NotFound;
