7a9675e3786d29a0d5c44aa4ca4e1dba0053c13f
Deepikakolli4 <deepikakolli2004@gmail.com>
2025-05-23 14:38:50 +0530

    added page not found

--- client/src/vendorDashBoard/components/NotFound/NotFound.js
+++ client/src/vendorDashBoard/components/NotFound/NotFound.js
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import gif from './giphy.gif';
+import './NotFound.css';
 const NotFound = () => {
   return (
     <div className='errorSection'>
         <h1>404</h1>
-        <div>Page not found</div>
+         <div className="gifContainer">
+                <img src={gif} alt="pagenotfound" />
+          </div>
     </div>
   )
 }
